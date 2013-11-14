3bfe2e13e4fc7b2f88105269748983f79ed07f98
curtis Allen <curtisaallen@gmail.com>
2013-11-13 20:41:39 -0500

    add create new form to app

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,13 +9,27 @@ class MoviesController < ApplicationController
 	end
 
 	def edit
-     @movie = Movie.find(params[:id])
+     	@movie = Movie.find(params[:id])
     end
 
     def update
       @movie = Movie.find(params[:id])
-      movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
-      @movie.update(movie_params)
+	  @movie.update(movie_params)
       redirect_to @movie
     end
+
+
+    def create
+        @movie = Movie.new(movie_params)
+    	@movie.save
+    	redirect_to @movie
+    end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+
+
 end
