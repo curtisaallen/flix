ea72ae10a4b5c3c74af149707d31ccddf22d0c68
curtis Allen <curtisaallen@gmail.com>
2013-11-16 22:20:43 -0500

    Added destory method to app

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,6 +29,13 @@ class MoviesController < ApplicationController
   	end
 
 
+	 def destroy
+		 @movie = Movie.find(params[:id])
+		 @movie.destroy
+		 redirect_to movies_url
+	 end 	
+
+
 private
 
   def movie_params
