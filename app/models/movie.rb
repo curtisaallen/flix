0bcff88f148c8df5cd57dd43bbad8721430ec1f6
curtis Allen <curtisaallen@gmail.com>
2013-11-18 20:54:21 -0500

    custom querie for movie app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,10 @@ class Movie < ActiveRecord::Base
 	def flop
        totall_gross.blank? || totall_gross < 50000000
 	end
+
+
+    def self.released
+    	where("released_on <= ?", Time.now).order("released_on desc")
+    end
+
 end
