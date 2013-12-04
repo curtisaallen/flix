bccef08ad5e98dc83c2d784e12dda8cacd8e2a31
curtis Allen <curtisaallen@gmail.com>
2013-12-03 22:41:30 -0500

    add validations to app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,17 @@
 class Movie < ActiveRecord::Base
+   
+   validates :tile, :released_on, :duration, presence: true
+   validates :description, length: { minimum: 25 }
+   validates :totall_gross, numericality: { greater_than_or_equal_to: 0}
+   validates :image_file_name, allow_blank: true, format: {
+	  with:    /\w+.(gif|jpg|png)\z/i,
+	  message: "must reference a GIF, JPG, or PNG image"
+	}
+
+	RATINGS = %w(G PG PG-13 R NC-17)
+
+	validates :rating, inclusion: {in: RATINGS }
+
 	def flop
        totall_gross.blank? || totall_gross < 50000000
 	end
